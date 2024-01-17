787926f15a1cb8d0700967330d146e9df44778c7
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2024-01-17 00:19:28 -0300

    Get Health Factor and query getUserAccountData from contract

--- routes/address/[id]/[network].get.ts
+++ routes/address/[id]/[network].get.ts
@@ -2,14 +2,16 @@ import { ethers, formatEther } from "ethers";
 import { fetchQuery, init } from "@airstack/node";
 import axios from 'axios';
 import { config } from "dotenv";
+import { getUserAccountData } from "./getUserAccountData";
+import { networkProviders } from "./constants";
 config();
 
 init(process.env.AIRSTACK_API_KEY as string);
 
 export default eventHandler(async (event) => {
   const address = event.context.params.id
-  const network = event.context.params.network || 'Polygon'
-  const provider = ethers.getDefaultProvider('homestead')
+  const network = event.context.params.network || 'homestead'
+  const provider = ethers.getDefaultProvider(networkProviders[network.toLowerCase()])
 const query = `
 query MyQuery {
   Network: TokenBalances(
@@ -41,10 +43,11 @@ query MyQuery {
 `
   const { data: { ethereum: { usd: ETHUSD } } } = await axios.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd')
   const balance = await provider.getBalance(address);
+  const accountData = await getUserAccountData(network,address);
   const { data, error } = await fetchQuery(query, {});
   if(data){
   const filteredAAVETokens = data.Network?.TokenBalance?.filter((token:any) => token.token.name.includes('Aave'))
-  return { balance: formatEther(balance), filteredAAVETokens, res:'get the ETHUSD and multiply by the WETH and balance the user has to show it in dollars', ETHUSD , address, error}
+  return { balance: formatEther(balance), filteredAAVETokens, res:'get the ETHUSD and multiply by the WETH and balance the user has to show it in dollars', ETHUSD , address, error, accountData}
   }else {
     return {error};
   }
---
+++ routes/address/[id]/getUserAccountData.ts
@@ -0,0 +1,25 @@
+import abi from '../../../abis/Pool.json';
+import { ethers, formatEther, formatUnits } from "ethers";
+import { networkProviders } from './constants';
+
+export async function getUserAccountData(network:string, address: string) {
+  return new Promise((resolve, reject) => {
+
+  const provider = ethers.getDefaultProvider(networkProviders[network.toLowerCase()])
+const contractAddress = {
+      'Polygon': '0x794a61358D6845594F94dc1DB02A252b5b4814aD',
+      'ethereum': '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2',
+      'homestead': '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2'
+    };
+var aaveContract = new ethers.Contract(contractAddress[network], abi, provider);
+
+aaveContract.getUserAccountData(address).then(function(userAccountData) {
+    resolve({
+        totalCollateralBase: formatUnits(userAccountData.totalCollateralBase, 8),
+        totalDebtBase: formatUnits(userAccountData.totalDebtBase, 8),
+        availableBorrowsBase: formatUnits(userAccountData.availableBorrowsBase, 8),
+        healthFactor: formatEther(userAccountData.healthFactor),
+      })
+	})});
+
+}
