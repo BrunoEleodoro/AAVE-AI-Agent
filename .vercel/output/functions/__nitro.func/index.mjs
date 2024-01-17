c16f247e0acca8d82e43e8c60c1bbe7e06736d76
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2024-01-16 23:20:44 -0300

    remove build folder

--- .vercel/output/functions/__nitro.func/index.mjs
+++
@@ -1,6 +0,0 @@
-globalThis._importMeta_={url:import.meta.url,env:process.env};export { v as default } from './chunks/nitro/vercel.mjs';
-import 'node:http';
-import 'node:https';
-import 'fs';
-import 'path';
-//# sourceMappingURL=index.mjs.map
