d974f2ff75754dcab5f039b7bb573960b467f1e1
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2024-01-19 13:07:26 -0300

    safe protocol- kit integrated

--- routes/transaction/[id].get.ts
+++ routes/transaction/[id].get.ts
@@ -1,16 +1,21 @@
-import { EthersAdapter } from "@safe-global/protocol-kit";
-import { ethers } from "ethers";
+import Safe, { EthersAdapter } from '@safe-global/protocol-kit'
+import { ethers } from 'ethers';
 
-const provider = ethers.getDefaultProvider("homestead");
 
-export default eventHandler((event) => {
+export default eventHandler(async (event) => {
   const address = event.context.params.id;
+  const provider = ethers.getDefaultProvider("homestead");
+
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
 
-  const ethAdapter = new EthersAdapter({
-    ethers,
-    signerOrProvider: signer,
-  });
-  
-  return { nitro: "Transaction Builder!", address, ethAdapter };
+  const ethAdapter = new EthersAdapter({ ethers, signerOrProvider: signer})
+
+  const safeSdk: Safe = await (Safe as any).default.create({
+    safeAddress: address,
+    ethAdapter: ethAdapter,
+  })
+
+  console.log(await safeSdk.getOwners())
+
+  return { nitro: "Transaction Builder!", address };
 });
